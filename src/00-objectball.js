3bb3388cd60df0b7cc02f575eb56cf3af0d1c9db
Aruna <aruna.evan15@gmail.com>
2021-09-24 08:21:07 -0700

    commit tree dbfea7e49f03f3423082582a388576df7dde205a

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -5,14 +5,14 @@ function gameObject(){
             colors:['Black', 'White'],
             players:{
                 'Alan Anderson':{
-                number:0,
-                shoe:16,
-                points:22,
-                rebounds:12,
-                assists:12,
-                steals:3,
-                blocks:1,
-                slamDunks:1,
+                    number:0,
+                    shoe:16,
+                    points:22,
+                    rebounds:12,
+                    assists:12,
+                    steals:3,
+                    blocks:1,
+                    slamDunks:1,
                 },
                 'Reggie Evans':{
                     number:30,
@@ -156,4 +156,74 @@ function teamColors(teamName) {
     }
 }
 
-console.log(teamColors('Brooklyn Nets'))
+function teamNames() {
+
+    // const teamStats = gameObject();
+    // let resArray = [];
+    // resArray.push(teamStats.home.teamName);
+    // resArray.push(teamStats.away.teamName);
+    // return resArray;
+
+    return Object.entries(gameObject()).map((obj) => obj[1].teamName);
+}
+
+function playerNumbers(teamName) {
+    let teamStats = gameObject();
+    const homeTeam = teamStats.home;
+    const awayTeam = teamStats.away;
+
+    if (teamName = homeTeam.teamName) {
+        return numHelper(homeTeam.players);
+    }
+    if (teamName = awayTeam.teamName) {
+        return numHelper(awayTeam.players);
+    }
+
+    function numHelper(teamPlayers) {
+        return Object.entries(teamPlayers).map((obj) => obj[1].number);
+    }
+}
+
+function playerStats(playerName) {
+    let teamStats = gameObject();
+    const homePlayers = teamStats.home.players;
+    const awayPlayers = teamStats.away.players;
+
+    for (const player in homePlayers) {
+        if (player === playerName) {
+            return homePlayers[player];
+        }
+    }
+    for (const player in awayPlayers) {
+        if (player === playerName) {
+            return awayPlayers[player];
+        }
+    }
+}
+
+function bigShoeRebounds() {
+    let teamStats = gameObject();
+    const playersArray = Object.entries(teamStats.home.players).concat(Object.entries(teamStats.away.players));
+    const sortedPlayers = playersArray.sort((playerArray1, playerArray2)=>{
+            return playerArray1[1].shoe - playerArray2[1].shoe;
+    });
+
+    return sortedPlayers.pop()[1];
+}
+
+function mostPointsScored() {
+    let teamStats = gameObject();
+    const playersArray = Object.entries(teamStats.home.players)
+                            .concat(Object.entries(teamStats.away.players));
+    const sortedPlayers = playersArray.sort((playerArray1, playerArray2)=>{
+            return playerArray1[1].points - playerArray2[1].points;
+    });
+
+    return sortedPlayers.pop()[1];
+}
+
+
+
+console.log(mostPointsScored());
+
+// bigShoeRebounds();
